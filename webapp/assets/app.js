cee0eeb1e72a3987022688cc67a8718fa1d2149c
Matty <54020396+matty-ross@users.noreply.github.com>
2026-08-27 13:39:48 +0200

    Added Bootstrap icons.

--- webapp/assets/app.js
+++ webapp/assets/app.js
@@ -2,5 +2,6 @@ import './stimulus_bootstrap.js';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
 
 import './styles/app.css';
