7a7db98bd5db603411303125ff484d7ce23cd5be
Matty <54020396+matty-ross@users.noreply.github.com>
2026-08-28 20:14:21 +0200

    Fixed entrypoint shell script.

--- webapp/docker-entrypoint.sh
+++ webapp/docker-entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 set -e
 
-if [ "$APP_ENV" = "dev" ]; then
+IS_DEV=$([ "$APP_ENV" = "dev" ] && echo true || echo false)
+
+if [ "$IS_DEV" = true ]; then
     composer install --no-interaction
 else
     composer install --no-dev --no-interaction
-    php bin/console asset-map:compile --no-interaction
 fi
 
 php bin/console importmap:install --no-interaction
+
+if [ "$IS_DEV" = false ]; then
+    php bin/console asset-map:compile --no-interaction
+fi
+
 php bin/console doctrine:migrations:migrate --no-interaction
 
 exec docker-php-entrypoint "$@"
