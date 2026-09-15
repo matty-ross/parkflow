4196b1ecf492fb54e22ab6ef9ac45e2c93467043
Matty <54020396+matty-ross@users.noreply.github.com>
2026-09-15 20:08:36 +0200

    Removed unused translations + edited migrations.

--- webapp/migrations/Version20260915174504.php
+++ webapp/migrations/Version20260915174504.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20260826110718 extends AbstractMigration
+final class Version20260915174504 extends AbstractMigration
 {
     public function getDescription(): string
     {
